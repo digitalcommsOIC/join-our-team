8c9c2a243fb2e33a07330d5dd8bad24e88e2afbe
Murkuin <hudsonjohnson.visualmedia@gmail.com>
2024-10-05 09:34:48 +0100

    Refactor RecentPosts and index.astro to update latest posts link

--- src/components/RecentPosts.svelte
+++ src/components/RecentPosts.svelte
@@ -157,12 +157,14 @@
 
 <div class="flex justify-center px-8">
     <div class="xl:w-[80vw]">
-        <a href="/vacancies" data-astro-reload class="w-fit items-center text-[#1e1e1e] text-2xl lg:text-4xl font-bold text-pretty mb-6 flex flex-row gap-2 hover:animate-pulse group">latest posts
+        <!-- <a href="https://myjobscotland.gov.uk/councils/orkney-islands-council/jobs" data-astro-reload class="w-fit items-center text-[#1e1e1e] text-2xl lg:text-4xl font-bold text-pretty mb-6 flex flex-row gap-2 hover:animate-pulse group">latest posts
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 mt-[0.4rem] lg:mt-[0.75rem] group-hover:animate-pulse transition-all">
                 <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z" clip-rule="evenodd" />
                 <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z" clip-rule="evenodd" />
             </svg>
-        </a>
+        </a> -->
+
+        <h2 class="text-[#1e1e1e] text-2xl lg:text-4xl font-bold text-pretty mb-4">latest posts</h2>
 
         <div class="flex justify-center items-center mb-4">
             {#if isLoading}
