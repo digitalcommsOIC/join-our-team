18ffee08208e2d8aca1a2bf654ccb8fd5bc14705
Hudson Johnson <hudsonjohnson.visualmedia@gmail.com>
2026-02-02 09:28:07 +0000

    removal of head of edu page

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,18 +1,17 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CGUzIA-v.mjs';
-import { manifest } from './manifest_BL_yGrHa.mjs';
+import { manifest } from './manifest_DsIuFUv8.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
 const _page2 = () => import('./pages/about.astro.mjs');
 const _page3 = () => import('./pages/api/rss-feed.astro.mjs');
 const _page4 = () => import('./pages/benefits.astro.mjs');
-const _page5 = () => import('./pages/headofeducation.astro.mjs');
-const _page6 = () => import('./pages/iod.astro.mjs');
-const _page7 = () => import('./pages/recruitment-incentives.astro.mjs');
-const _page8 = () => import('./pages/resources.astro.mjs');
-const _page9 = () => import('./pages/vacancies.astro.mjs');
-const _page10 = () => import('./pages/index.astro.mjs');
+const _page5 = () => import('./pages/iod.astro.mjs');
+const _page6 = () => import('./pages/recruitment-incentives.astro.mjs');
+const _page7 = () => import('./pages/resources.astro.mjs');
+const _page8 = () => import('./pages/vacancies.astro.mjs');
+const _page9 = () => import('./pages/index.astro.mjs');
 
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/node.js", _page0],
@@ -20,12 +19,11 @@ const pageMap = new Map([
     ["src/pages/about.astro", _page2],
     ["src/pages/api/rss-feed.js", _page3],
     ["src/pages/benefits.astro", _page4],
-    ["src/pages/headofeducation.astro", _page5],
-    ["src/pages/iod.astro", _page6],
-    ["src/pages/recruitment-incentives.astro", _page7],
-    ["src/pages/resources.astro", _page8],
-    ["src/pages/vacancies.astro", _page9],
-    ["src/pages/index.astro", _page10]
+    ["src/pages/iod.astro", _page5],
+    ["src/pages/recruitment-incentives.astro", _page6],
+    ["src/pages/resources.astro", _page7],
+    ["src/pages/vacancies.astro", _page8],
+    ["src/pages/index.astro", _page9]
 ]);
 const serverIslandMap = new Map();
 const _manifest = Object.assign(manifest, {
